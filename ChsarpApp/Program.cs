a8c0e64e38b00067a02061a2a15fa4b34ece75ab
metinkuzey <metin.kuzey@outlook.com>
2021-11-21 03:12:19 +0300

    first commit

---
+++ ChsarpApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChsarpApp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
